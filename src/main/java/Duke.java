ec5b1881f47a023f404acfa1b1703f2877eb27ad
11019-15878\Leon <e0374068@u.nus.edu>
2022-04-12 23:32:27 +0800

    Added llist array

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,17 +5,28 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        String[] list = new String[100];
         boolean running = true;
         String input ;
+        int count = 0;
         System.out.println("Hello! Welcome to Chat bot! Please Type a command:");
+
         while(running == true){
-            input = scan.next();
+            input = scan.nextLine();
             if (input.equals("bye")){
                 running = false;
                 System.out.println("Thank you for using Chat bot! Goodbye!");
 
+            }else if(input.equals("list")){
+                System.out.println("Your List:");
+                for(int i = 1; i<=count; i++)
+                    System.out.println( i + ". " +list[i-1]);
+
             }else{
-                System.out.println(input);
+                list[count]= input;
+                System.out.println("Added:"+ list[count]);
+                count++;
+
             }
         }
     }
